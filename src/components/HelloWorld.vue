f86d1b70aac5df4ed2bc25dcfddaf01c49085d47
Sara Kostovska <sara.kostovska@preskok.si>
2023-12-14 13:22:00 +0100

    Vue/compat testing with & without vuetify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,28 +9,17 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="d-flex justify-center">
+    <v-card width="300px">
+      <v-card-title class="text-h6 text-md-h5 text-lg-h4">Title</v-card-title>
+      <v-card-text>
+        Body text
+      </v-card-text>
+    </v-card>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+ 
 </template>
 
 <style scoped>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,27 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      // template: {
+      //   transformAssetUrls
+      // }
+    }),
+    // Vue migration build is incompatible with vuetify, 
+    // even with the vuetify latest
+    // vue({
+    //   template: {
+    //     compilerOptions: {
+    //         compatConfig: {
+    //             MODE: 2
+    //         }
+    //     }
+    //   }
+    // })
+  ],
+  // resolve: {
+  //   alias: {
+  //     vue: '@vue/compat',
+  //   },
+  // }
 })
